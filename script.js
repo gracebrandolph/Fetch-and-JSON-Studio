30817667e53268e9ba90a17c0ef71b60177b4068
gracebrandolph <56567886+gracebrandolph@users.noreply.github.com>
2019-11-06 19:52:46 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        let container = document.getElementById('container');
+        response.json().then(function(json) {
+            let myString = "";
+            for(let i = 0; i < json.length; i++){
+                myString += `<div class="astronaut"><div class = "bio"><h3>${json[i].firstName} ${json[i].lastName}
+                </h3><ul><li>Hours in space: ${json[i].hoursInSpace}</li><li>Active: ${json[i].active}</li><li>Skills: 
+                ${json.skills}</li></ul></div><img class="avatar" src="${json[i].picture}"></div>`;
+            }
+            container.innerHTML = myString;
+         });
+            
+    });
+});
